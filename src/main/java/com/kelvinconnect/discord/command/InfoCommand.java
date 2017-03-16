ce0146db737db34902cb9cb4d729f6ce49bf11f2
Adam Docherty <uklooneyjr@hotmail.co.uk>
2017-03-16 00:23:03 +0000

    Added link to git repo to info, also got rid of most of the other stuff info does as it was pointless. see #5

--- src/main/java/com/kelvinconnect/discord/command/InfoCommand.java
+++ src/main/java/com/kelvinconnect/discord/command/InfoCommand.java
@@ -13,28 +13,10 @@ import java.util.Date;
  */
 public class InfoCommand implements CommandExecutor {
 
-    @Command(aliases = "!info", description = "Shows some information about the bot.", usage = "!info [author|time]")
+    @Command(aliases = "!info", description = "Shows some information about the bot.", usage = "!info")
     public String onInfoCommand(String[] args) {
-        if (args.length > 1) { // more than 1 argument
-            return "Too many arguments!";
-        }
-        if (args.length == 0) { // !info
-            return "- **Author:** Adam Docherty\n" +
-                    "- **Language:** Java\n" +
-                    "- **Command-Lib:** sdcf4j";
-        }
-        if (args.length == 1) { // 1 argument
-            if (args[0].equals("author")) { // !info author
-                return "- **Name:** Adam Docherty\n" +
-                        "- **Age:** 22\n" +
-                        "- **Favourite Food:** Mac 'n' Cheese";
-            }
-            if (args[0].equals("time")) { // !info time
-                SimpleDateFormat format = new SimpleDateFormat("HH:mm");
-                Date currentDate = new Date(System.currentTimeMillis());
-                return "It's " + format.format(currentDate);
-            }
-        }
-        return "Unknown argument!";
+        return "- **Author:** Adam Docherty\n" +
+                "- **Language:** Java\n" +
+                "- **Source:** http: https://bitbucket.org/AdamDocherty/kc-discord-bot/";
     }
 }
