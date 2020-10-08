bc95d7b1d8429aaab7d7284f9299f821eee0bfd6
Adam Docherty <uklooneyjr@gmail.com>
2020-10-08 16:49:27 +0100

    see #16 Adding the current bot version to the `!info` command

--- src/main/java/com/kelvinconnect/discord/command/InfoCommand.java
+++ src/main/java/com/kelvinconnect/discord/command/InfoCommand.java
@@ -2,6 +2,11 @@ package com.kelvinconnect.discord.command;
 
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Displays info about the bot
@@ -9,10 +14,32 @@ import de.btobastian.sdcf4j.CommandExecutor;
  * Created by Adam on 14/03/2017.
  */
 public class InfoCommand implements CommandExecutor {
+    private static final Logger logger = LogManager.getLogger(InfoCommand.class);
+
+    private String projectVersion;
+
+    public String getProjectVersion() {
+        if (null == projectVersion) {
+            final Properties properties = new Properties();
+            try {
+                properties.load(getClass().getClassLoader().getResourceAsStream(".properties"));
+            } catch (IOException e) {
+                logger.warn("Error loading .properties file from class loader", e);
+            }
+            projectVersion = properties.getProperty("version");
+            if (null == projectVersion) {
+                projectVersion = "Unknown";
+            }
+        }
+
+        return projectVersion;
+    }
 
     @Command(aliases = "!info", description = "Shows some information about the bot.", usage = "!info")
     public String onInfoCommand(String[] args) {
-        return "- **Author:** Adam Docherty\n" + "- **Language:** Java\n"
-                + "- **Source:** https://github.com/UKLooneyJr/kc-discord-bot/";
+        return "- **Version:** " + getProjectVersion() + "\n" +
+               "- **Author:** Adam Docherty\n" +
+               "- **Language:** Java\n" +
+               "- **Source:** https://github.com/UKLooneyJr/kc-discord-bot/";
     }
 }
