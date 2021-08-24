23eb9e4a64d310e22e5665c894ec424cfcb9de5e
Sam Stafford <samuel.stafford@motorolasolutions.com>
2021-08-24 13:39:02 +0100

    feat: now reading channels from an xml channel list
    This will only work as-is once it's merged into UKLooneyJr's repo (due to the github URL)

--- src/main/java/com/kelvinconnect/discord/command/JoinLeaveCommand.java
+++ src/main/java/com/kelvinconnect/discord/command/JoinLeaveCommand.java
@@ -13,7 +13,15 @@ import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -22,11 +30,21 @@ public class JoinLeaveCommand implements CommandExecutor {
 
     private static final String INVALID_CHANNEL_NAME = "Invalid channel name, try '!channels' for a list of all channels.";
     private static final String CANT_LEAVE_CHANNEL = "Sorry, you can't leave this channel.";
+    private static final long KC_SERVER_ID = 239013363387072514L;
+
+    private static final String KC_CHANNEL_LIST_URL = "https://raw.githubusercontent.com/UKLooneyJr/kc-discord-bot/master/resources/channels.xml";
+    private static final String KC_CHANNEL_LIST_ELEMENT = "KCChannelList";
+    private static final String KC_CHANNEL_ELEMENT = "KCChannel";
+    private static final String CHANNEL_ID_NODE = "ChannelId";
+    private static final String ROLE_ID_NODE = "RoleId";
+    private static final String ALIASES_NODE = "Aliases";
+    private static final String ALIAS_NODE = "Name";
+
     private final Server kcServer;
-    private List<KCChannel> channels;
+    private final List<KCChannel> channels = new ArrayList<>();
 
     public JoinLeaveCommand(DiscordApi api) {
-        kcServer = api.getServerById(239013363387072514L)
+        kcServer = api.getServerById(KC_SERVER_ID)
                 .orElseThrow(() -> new RuntimeException("Failed to find KC server."));
         initChannels();
     }
@@ -40,26 +58,77 @@ public class JoinLeaveCommand implements CommandExecutor {
     }
 
     private void initChannels() {
-        channels = Arrays.asList(new KCChannel(276318041443270657L, 763082445524041761L, "pubchat"),
-                new KCChannel(689418031151054944L, 763085327270543381L, "corona", "covid"),
-                new KCChannel(550614902532997131L, 763085269435678791L, "spamdo"),
-                new KCChannel(421223895513956352L, 421225268057735168L, "music"),
-                new KCChannel(694117175392469032L, 763083545451167774L, "film", "tv"),
-                new KCChannel(408933031064371200L, 763083371193696267L, "roll", "role"),
-                new KCChannel(425314799455436801L, 425314649458737163L, "anime", "weeb"),
-                new KCChannel(763082945979744278L, 763082196985970709L, "pets"),
-                new KCChannel(770330765133086751L, 770330666411360299L, "food"),
-                new KCChannel(763091730605408307L, 763091623386021888L, "programming"),
-                new KCChannel(763091960541478912L, 763091847559118868L, "botdev", "kcbot"),
-                new KCChannel(763083029794390017L, 763082257429692458L, "tech"),
-                new KCChannel(763083100594110524L, 763082324748533790L, "sport"),
-                new KCChannel(763086481560305704L, 763086365835395123L, "politics"),
-                new KCChannel(421623339145232404L, 421623272434565130L, "games"),
-                new KCChannel(763089542152323112L, 763089285632884756L, "tetris"),
-                new KCChannel(753587144991178792L, 753586719999393872L, "lol"),
-                new KCChannel(763082784784515092L, 763082509952876546L, "wow"),
-                new KCChannel(874359730540253204L, 874359350209151046L, "ffxiv", "ff14"));
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+            URLConnection urlConnection = new URL(KC_CHANNEL_LIST_URL).openConnection();
+            urlConnection.addRequestProperty("Accept", "application/xml");
+            Document channelListDocument = documentBuilder.parse(urlConnection.getInputStream());
+            String root = channelListDocument.getDocumentElement().getNodeName();
+            if (root.equals(KC_CHANNEL_LIST_ELEMENT)) {
+                Node rootNode = channelListDocument.getDocumentElement();
+                loadFromXml(rootNode);
+            } else {
+                loadFromDefaults();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            loadFromDefaults();
+        }
+
     }
+
+    private void loadFromXml(Node rootNode) {
+        //NodeList not iterable for some reason
+
+        for (int i = 0; i < rootNode.getChildNodes().getLength(); i++) {
+            Node thisNode = rootNode.getChildNodes().item(i);
+            if (thisNode instanceof Element) {
+                Element channelElement = (Element) thisNode;
+                if (channelElement.getNodeName().equals(KC_CHANNEL_ELEMENT)) {
+                    Element channelIdElement = (Element) channelElement.getElementsByTagName(CHANNEL_ID_NODE).item(0);
+                    long channelId = Long.parseLong(channelIdElement.getTextContent());
+                    Element roleIdElement = (Element) channelElement.getElementsByTagName(ROLE_ID_NODE).item(0);
+                    long roleId = Long.parseLong(roleIdElement.getTextContent());
+                    Element aliasesElement = (Element) channelElement.getElementsByTagName(ALIASES_NODE).item(0);
+                    NodeList aliasList = aliasesElement.getElementsByTagName(ALIAS_NODE);
+                    String[] aliases = new String[aliasList.getLength()];
+                    for (int j = 0; j < aliasList.getLength(); j++) {
+                        aliases[j] = (aliasList.item(j).getTextContent());
+                    }
+                    KCChannel newChannel = new KCChannel(channelId, roleId, aliases);
+                    channels.add(newChannel);
+                }
+            }
+        }
+    }
+
+    private void loadFromDefaults() {
+        if (channels.isEmpty()) {
+            channels.addAll(Arrays.asList(
+                    new KCChannel(276318041443270657L, 763082445524041761L, "pubchat"),
+                    new KCChannel(689418031151054944L, 763085327270543381L, "corona", "covid"),
+                    new KCChannel(550614902532997131L, 763085269435678791L, "spamdo"),
+                    new KCChannel(421223895513956352L, 421225268057735168L, "music"),
+                    new KCChannel(694117175392469032L, 763083545451167774L, "film", "tv"),
+                    new KCChannel(408933031064371200L, 763083371193696267L, "roll", "role"),
+                    new KCChannel(425314799455436801L, 425314649458737163L, "anime", "weeb"),
+                    new KCChannel(763082945979744278L, 763082196985970709L, "pets"),
+                    new KCChannel(770330765133086751L, 770330666411360299L, "food"),
+                    new KCChannel(763091730605408307L, 763091623386021888L, "programming"),
+                    new KCChannel(763091960541478912L, 763091847559118868L, "botdev", "kcbot"),
+                    new KCChannel(763083029794390017L, 763082257429692458L, "tech"),
+                    new KCChannel(763083100594110524L, 763082324748533790L, "sport"),
+                    new KCChannel(763086481560305704L, 763086365835395123L, "politics"),
+                    new KCChannel(421623339145232404L, 421623272434565130L, "games"),
+                    new KCChannel(763089542152323112L, 763089285632884756L, "tetris"),
+                    new KCChannel(753587144991178792L, 753586719999393872L, "lol"),
+                    new KCChannel(763082784784515092L, 763082509952876546L, "wow"),
+                    new KCChannel(874359730540253204L, 874359350209151046L, "ffxiv", "ff14"))
+            );
+        }
+    }
+
 
     @Command(aliases = "!join", description = "Joins a channel.", usage = "!join [<channel-name>]")
     public void onJoinCommand(String[] args, DiscordApi api, Message message) {
